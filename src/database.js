c74d625daf7b36d454abe1dc54267d223bf5eef1
Daniel Bustos Ruiz <daniel.bustos935@hotmail.com>
2022-08-12 17:36:55 -0500

    Removed some comment lines from the database file

--- src/database.js
+++ src/database.js
@@ -1,5 +1,4 @@
 const mysql = require('promise-mysql');
-//const { promisify } = require('util');
 
 const connection = mysql.createConnection({
 
@@ -15,31 +14,3 @@ function getConnection(){
 }
 
 module.exports = { getConnection }
-
-// const pool = mysql.createPool({
-
-//     host: process.env.HOST_DB,
-//     user: process.env.USER_DB,
-//     password: process.env.PASSWORD_DB,
-//     database: process.env.DATABASE
-
-// });
-
-// pool.getConnection((error, connection)=>{
-
-//     if(error){
-//         console.log(error);
-//     }else{
-
-//         if(connection){
-//             connection.release();
-//         }
-
-//     }
-//     return;
-
-// });
-
-// pool.query = promisify(pool.query);
-
-// module.exports = pool;
